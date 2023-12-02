5d6dfed1944b7b8a059af079ec46e8b97fd7208e
Rogerio <rogeriopiatek@gmail.com>
2023-12-01 21:54:20 -0300

    feat: ensure animations on home and cards page

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
     fontWeight: fonts.fontWeights,
     fontFamily: {
       sans: fonts.fontFamilies.default,
+      mono: "monospace",
     },
     fontSize: fonts.fontSizes,
     lineHeight: fonts.lineHeights,
